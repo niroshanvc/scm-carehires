cf76de50b7ad0bd83332d277f8e8e4cc4455bd7e
Niroshan - CareHires <niroshan.desilva@carehires.com>
2024-09-13 17:39:23 +0530

    commented out initDriver method in Hooks class

--- src/test/java/com/carehires/hooks/Hooks.java
+++ src/test/java/com/carehires/hooks/Hooks.java
@@ -20,4 +20,39 @@ public class Hooks {
         scenario.attach(screenshot, "image/png", scenario.getName());
         BasePage.tearDown();
     }
+
+    /*public static void initDriver() {
+        String browser = System.getProperty("BROWSER");
+        if (browser == null) {
+            browser = BasePage.getProperty("BROWSER");
+        }
+
+        if (browser.equalsIgnoreCase("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            BasePage.driver = new FirefoxDriver();
+            BasePage.driver.manage().window().maximize();
+        } else if (browser.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().clearDriverCache().setup();
+            WebDriverManager.chromedriver().clearResolutionCache().setup();
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+            if (System.getenv("CI") != null) {
+                BasePage.logger.info("Running in CircleCI, configuring Chrome for headless execution.");
+                options.addArguments("--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage");
+            }
+
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--start-maximized");
+            options.addArguments("--disable-notifications");
+
+            BasePage.driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            BasePage.driver = new EdgeDriver();
+            BasePage.driver.manage().window().maximize();
+        } else {
+            BasePage.logger.info("Browser name %s is invalid.", browser);
+        }
+    }*/
 }
