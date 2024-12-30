a8774944caa42960820383e02d847e4340a520e0
Niroshan - CareHires <niroshan.desilva@carehires.com>
2024-12-30 20:56:33 +0530

    enable test run in circle ci

--- src/test/java/com/carehires/runner/CucumberRunnerTests.java
+++ src/test/java/com/carehires/runner/CucumberRunnerTests.java
@@ -2,7 +2,6 @@ package com.carehires.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -15,9 +14,4 @@ import org.testng.annotations.DataProvider;
 
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
 }
